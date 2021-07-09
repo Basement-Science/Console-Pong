f8d793f6f16897589b14d8f1303e3340474ff98a
Basement Science <BaseSc@protonmail.com>
2021-07-10 00:57:52 +0200

    keyboard implementation using SharpDX.DirectInput

--- Ball.cs
+++ Ball.cs
@@ -1,11 +1,22 @@
-﻿namespace ConsolePong {
-    internal class Ball {
-        private object x;
-        private object y;
+﻿using System;
+using System.Numerics;
 
-        public Ball(object x, object y) {
+namespace ConsolePong {
+    internal class Ball {
+        private int x;
+        private int y;
+        private char symbol = 'O';
+        Vector2 moveVector = new();
+
+        private static Random random = new();
+        
+        public Ball(int x, int y) {
             this.x = x;
             this.y = y;
+
+            double angle = random.NextDouble() * Math.PI;
+            moveVector.X = (float)(Math.Cos(angle) - 1.0 * Math.Sin(angle));
+            moveVector.Y = (float)(Math.Sin(angle) + 1.0 * Math.Cos(angle));
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Threading;
 using System.Threading.Tasks;
+using SharpDX.DirectInput;
 
 namespace ConsolePong {
     class Program {
         private static GameField gameField = new(25, 120);
+        private static Keyboard keyboard = new Keyboard(new DirectInput());
+
         static async Task Main(string[] args) {
             var inputHandler = new Task(ReadKeys);
             var outputHandler = new Task(PrintGameField);
 
             Console.CancelKeyPress += new ConsoleCancelEventHandler(ExitHandler);
             Console.CursorVisible = false;
+
+            keyboard.Properties.BufferSize = 128;
+            keyboard.Acquire();
 
             inputHandler.Start();
             outputHandler.Start();
@@ -21,28 +27,102 @@ namespace ConsolePong {
             e.Cancel = true;
         }
 
-        static void ReadKeys() {
-            for (ConsoleKeyInfo keyInfo = new(); 
-                keyInfo.Key != ConsoleKey.Escape;
-                keyInfo = Console.ReadKey(true)) {
+        class ActionSpammer {
+            public Key assignedKey { get; private set; }
+            private Task task;
+            private Action action;
 
-                switch (keyInfo.Key) {
-                    case ConsoleKey.DownArrow:
-                        gameField.Move(gameField.player_2, Orientation.Direction.DOWN);
-                        break;
-                    case ConsoleKey.UpArrow:
-                        gameField.Move(gameField.player_2, Orientation.Direction.UP);
-                        break;
-                    case ConsoleKey.S:
-                        gameField.Move(gameField.player_1, Orientation.Direction.DOWN);
-                        break;
-                    case ConsoleKey.W:
-                        gameField.Move(gameField.player_1, Orientation.Direction.UP);
-                        break;
-                    default:
-                        break;
+            private volatile bool run = false;
+
+            private void spam() {
+                while (run) {
+                    action.Invoke();
+                    Thread.Sleep(50);
                 }
             }
+
+            public void startSpam() {
+                if (task.Status == TaskStatus.Created) {
+                    run = true;
+                    task.Start();
+                } else {
+                    throw new ThreadInterruptedException("something wrong with spammer for " + assignedKey);
+                }
+            }
+
+            public async void stopSpam() {
+                run = false;
+                await task;
+                task.Dispose();
+                task = new Task(spam);
+            }
+
+            public ActionSpammer(Key key, Action action) {
+                assignedKey = key;
+                this.action = action;
+                task = new Task(spam);
+            }
+        }
+
+        static void ReadKeys() {
+            Action action_W = new Action(() => {
+                gameField.Move(gameField.player_1, Orientation.Direction.UP);
+            });
+            Action action_S = new Action(() => {
+                gameField.Move(gameField.player_1, Orientation.Direction.DOWN);
+            });
+            Action action_Up = new Action(() => {
+                gameField.Move(gameField.player_2, Orientation.Direction.UP);
+            });
+            Action action_Down = new Action(() => {
+                gameField.Move(gameField.player_2, Orientation.Direction.DOWN);
+            });
+
+            ActionSpammer spammer_W = new ActionSpammer(Key.W, action_W);
+            ActionSpammer spammer_S = new ActionSpammer(Key.S, action_S);
+            ActionSpammer spammer_Up = new ActionSpammer(Key.Up, action_Up);
+            ActionSpammer spammer_Down = new ActionSpammer(Key.Down, action_Down);
+
+            while (true) {
+                keyboard.Poll();
+                var buffer = keyboard.GetBufferedData();
+                foreach (KeyboardUpdate update in buffer) {
+                    
+                    switch (update.Key) {
+                        case Key.W:
+                            if (update.IsPressed) {
+                                spammer_W.startSpam();
+                            } else if (update.IsReleased) {
+                                spammer_W.stopSpam();
+                            }
+                            break;
+                        case Key.S:
+                            if (update.IsPressed) {
+                                spammer_S.startSpam();
+                            } else if (update.IsReleased) {
+                                spammer_S.stopSpam();
+                            }
+                            break;
+                        case Key.Up:
+                            if (update.IsPressed) {
+                                spammer_Up.startSpam();
+                            } else if (update.IsReleased) {
+                                spammer_Up.stopSpam();
+                            }
+                            break;
+                        case Key.Down:
+                            if (update.IsPressed) {
+                                spammer_Down.startSpam();
+                            } else if (update.IsReleased) {
+                                spammer_Down.stopSpam();
+                            }
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            return;
         }
 
         static void PrintGameField() {
