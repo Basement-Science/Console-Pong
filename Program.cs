b952e92ba863f66e005b2e91eb441edcb155b1e7
Basement Science <BaseSc@protonmail.com>
2021-07-09 13:42:51 +0200

    initial commit, async read/write

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsolePong {
+    class Program {
+        static async Task Main(string[] args) {
+            _ = Task.Run(() => {
+                while (true) {
+                    Console.ReadKey(true);
+                    Console.WriteLine("Andere ausgabe!");
+                }
+            });
+
+            await Task.Run(() => {
+                while (true) {
+                    Console.WriteLine("Hello World!");
+                    Thread.Sleep(100);
+                }
+
+            });
+        }
+    }
+}
