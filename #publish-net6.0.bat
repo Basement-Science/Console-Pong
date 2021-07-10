b96298ea8d970542e98e2c630e3ecd6b8ed31bd4
Basement Science <BaseSc@protonmail.com>
2021-07-11 01:35:36 +0200

    Update publish script

--- #publish-net6.0.bat
+++ #publish-net6.0.bat
@@ -1,10 +1,11 @@
 @echo off
 color 0A
 call :publish win-x64
+call :publish win-x86
 timeout 20
 
 goto :end
 :publish
 dotnet.exe publish --self-contained true --configuration Release --framework net6.0 --output .\bin\#publish\net6.0-%1 --runtime %1
-REN ".\bin\#publish\net6.0-%1\Timestamp*" "Timestamp-%1*"
+REN ".\bin\#publish\net6.0-%1\ConsolePong*" "ConsolePong-%1*"
 :end
