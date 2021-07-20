0e6ef3e9a634164e85ab1417b39cda15f8571898
Basement Science <BaseSc@protonmail.com>
2021-07-20 08:01:55 +0200

    changed publish build script and config so that published exe works again.

--- #publish-net6.0.bat
+++ #publish-net6.0.bat
@@ -1,11 +1,12 @@
 @echo off
 color 0A
+set dotnetVersion=6.0
 call :publish win-x64
 call :publish win-x86
 timeout 20
 
 goto :end
 :publish
-dotnet.exe publish --self-contained true --configuration Release --framework net6.0 --output .\bin\#publish\net6.0-%1 --runtime %1
-REN ".\bin\#publish\net6.0-%1\ConsolePong*" "ConsolePong-%1*"
+dotnet.exe publish --self-contained true --configuration Release --framework net%dotnetVersion% --output .\bin\#publish\net%dotnetVersion%-%1 --runtime %1
+::REN ".\bin\#publish\net%dotnetVersion%-%1\ConsolePong*" "ConsolePong-%1*"
 :end
